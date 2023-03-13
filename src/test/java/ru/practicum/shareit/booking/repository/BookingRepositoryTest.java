17ae8c086d9b9e46a4877383a56f0f3b258c8d8f
Elena_B <chaldina.e@gmail.com>
2023-03-13 15:43:21 +0000

    больше тестов

--- src/test/java/ru/practicum/shareit/booking/repository/BookingRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/booking/repository/BookingRepositoryTest.java
@@ -34,8 +34,8 @@ class BookingRepositoryTest {
     PageRequest pageRequest = PageRequest.of(0, 10);
     User userInputElena = new User(userIdElena, "Elena", "chaldina.e@gmail.com");
     Item item = new Item(itemId, "name", "desc", true, userInputElena);
-    Booking booking = new Booking(LocalDateTime.now().withNano(0), LocalDateTime.now().
-            withNano(0).plusDays(1),
+    Booking booking = new Booking(LocalDateTime.now().withNano(0), LocalDateTime.now()
+            .withNano(0).plusDays(1),
             item, userInputElena, Status.WAITING, null);
 
     Booking pastBooking = new Booking(LocalDateTime.now().minusDays(1), LocalDateTime.now().minusDays(2),
